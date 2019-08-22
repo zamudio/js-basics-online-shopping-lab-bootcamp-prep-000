692cc5afca5ab80b23a27d6fce5e8ad03668d6e9
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-22 02:25:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,31 +34,9 @@ function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty"
   }
-  else if (cart.length === 1) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
+  else {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   }
-  else if (cart.length === 2)
-  
-
-  var array = [];
-
-  for ( let i = 0; i < cart.length; i++) {
-      array.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
-  }
-  if (cart.length === 0) {
-    return `Your shopping cart is empty.`;}
-
-  if (cart.length === 1){
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
-  }
-
-  if (cart.length ===2){
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
-  }else{
-    let lastItem = array.pop();
-    return `In your cart, you have ${array.join(', ')}, and ${lastItem}.`
-  }
-}
 }
 
 function total() {
