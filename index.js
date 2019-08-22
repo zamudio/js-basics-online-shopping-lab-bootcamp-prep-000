ea72155bb766939b6f61224b128696e8042abf13
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-22 02:35:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,9 +64,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  let totalCost = total()
   if (cardNumber) {
     cart = []
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
   }
   else {
     return "Sorry, we don't have a credit card on file for you."
