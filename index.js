27afa88d1a1db4c1a60a98623788906c14728983
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-22 02:15:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ function viewCart() {
   //loop over every item in cart
   //return as `In your cart, you have ${itemName} at //${itemPrice}, ... .`
   //syntax is important for handling 1-item cart, 2-item cart, //3item cart
+  
+  
 }
 
 function total() {
