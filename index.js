9191f3bec329ae46cad186dbe3874f83a349ae99
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-22 02:34:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function viewCart() {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   }
   else {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`
   }
 }
 
